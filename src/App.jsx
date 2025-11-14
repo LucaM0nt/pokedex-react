d5905a8d5b44e83d56362254e5bdccedbc6dba38
Luca Montanaro <lucamontanaromail@gmail.com>
2025-11-14 10:29:55 +0100

    fix: corretti errori merge

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { useGetAllPokemonQuery } from "./store/pokeApiSlice";
 import siteRoutes from "./siteRoutes"
 
 import './App.css'
 
 function App() {
   const routes = createBrowserRouter(siteRoutes)
+  const { data, error, isLoading } = useGetAllPokemonQuery(); 
 
   return (
     <>
-      <RouterProvider router={routes} />
       <ul>
-        {data.results.map((pokemon) => (
+        {data?.results?.map((pokemon) => (
           <li key={pokemon.name}>{pokemon.name}</li>
         ))}
       </ul>
+      <RouterProvider router={routes} />
     </>
   )
 }
