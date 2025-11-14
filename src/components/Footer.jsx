3301acbca9c6229ca7a28d8c22ed0400a5446ef5
Gloria Paita <Gloria.Paita@edu-its.it>
2025-11-14 11:30:56 +0100

    Modified footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,14 @@ export default function Footer() {
   return (
     <footer className='bg-primary rounded-t-xl mt-20'>
       <div className='container text-primary text-md text-center py-20'>
-        Copyright © {new Date().getFullYear()} - Pokédex React Project. All right reserved
+        <p>Site design and layout by Luca Montanaro and Gloria Paita.</p>
+        <a href="/privacy-policy.html"  className="text-blue-400" >Privacy Policy</a> - <a href="/terms-of-service.html" className="text-blue-400">Terms of Service</a> - <a href="/contact-us.html" className="text-blue-400">Contact us</a>
+        <p>Pokémon images, names, forms, and other relevant information are the property of The Pokémon Company.</p>
+      </div>
+      <div className='container text-primary text-md text-center py-10'>
+        <p>
+          Copyright © {new Date().getFullYear()} - Pokédex React Project.
+        </p>
       </div>
     </footer>
   )
