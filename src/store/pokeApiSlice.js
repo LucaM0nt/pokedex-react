1d772d416300a628f07339045e3c20137375a156
Luca Montanaro <lucamontanaromail@gmail.com>
2025-11-17 10:29:11 +0100

    fix: added tags to pokeapi slice

--- src/store/pokeApiSlice.js
+++ src/store/pokeApiSlice.js
@@ -3,12 +3,15 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const pokeApi = createApi({
     reducerPath: "pokeApi",
     baseQuery: fetchBaseQuery({ baseUrl: "https://pokeapi.co/api/v2/" }),
+    tagTypes: ['Pokemon'],
     endpoints: (builder) => ({
         getAllPokemon: builder.query({
             query: ({ limit = 30, offset = 0 } = {}) => `pokemon?limit=${limit}&offset=${offset}`,
+            providesTags: ['Pokemon'],
         }),
         getPokemon: builder.query({
             query: ( id ) => `pokemon/${id}`,
+            providesTags: ['Pokemon'],
         }),
     }),
 });
