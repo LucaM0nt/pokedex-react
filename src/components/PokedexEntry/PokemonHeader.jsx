16f695271c4f222f8366c597c648f23ea13f49b3
luca montanaro <lucamontanaromail@gmail.com>
2025-11-25 22:53:20 +0100

    minor fixes

--- src/components/PokedexEntry/PokemonHeader.jsx
+++ src/components/PokedexEntry/PokemonHeader.jsx
@@ -1,17 +1,11 @@
 import TypeTag from "../TypeTag";
 
-export default function PokemonHeader({ id, name, types }) {
+export default function PokemonHeader({ id, name }) {
   return (
     <div className="text-center mb-4">
-      <h2 className="text-2xl sm:text-3xl md:text-4xl font-black tracking-wide text-gray-800">
-        #{id}
-        <span className="block">{name.toUpperCase()}</span>
+      <h2 className="text-2xl sm:text-3xl md:text-4xl font-bold text-gray-800">
+        #{id} {name.toUpperCase()}
       </h2>
-      <div className="mt-2 flex justify-center flex-wrap gap-2">
-        {types.map((t) => (
-          <TypeTag key={t.type.name} type={t.type.name} />
-        ))}
-      </div>
     </div>
   );
 }
